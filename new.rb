87710d64a6c7a15039489f52c516b423978b4ae0
ravimathya <ravimathya@gmail.com>
2019-09-12 17:07:25 +0545

    another method in new.rb file code

--- new.rb
+++ new.rb
@@ -1,10 +1,8 @@
-$a = {}
 File.open("Unique.txt", "r+").each do |line|
-
     # file.each { |line| puts line.length }
     $b = line.split
-    $a[$b[0]] = $b[1]
 end
+puts $b[1]
 
 def generate_code(number)
     charset = Array('A'..'Z') + Array('a'..'z') + Array(0..9)
@@ -12,9 +10,8 @@ def generate_code(number)
 end
 
 def loop(url)
-    if $a.include?(url) then
-		code = $a[url]
-		puts "Unique code is #{code}."
+    if $b[0].include?(url) 
+		puts "Unique code is #{$b[1]}."
 		puts "press 1 to continue... or press enter to exit"
 		option = gets.chomp.to_i
 		if option == 1
@@ -22,9 +19,11 @@ def loop(url)
 			url = gets.chomp
 			puts loop(url)
 		end
-	else
-		$a["#{url}"] = "#{generate_code(8)}"
-		puts "Unique code is " + $a["#{url}"]
+    else
+        file = File.open("Unique.txt", "a")
+        file.puts "#{url}" + " " + "#{generate_code(8)}"
+        file.close
+		
 		puts "press 1 to continue... or press enter to exit"
 		option = gets.chomp.to_i
 		if option == 1
